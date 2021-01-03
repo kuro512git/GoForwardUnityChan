8327c3d6f7181775947095bc49f6550cc6430c13
Yuki Ishiguro <kuro512@gmail.com>
2021-01-03 13:57:09 +0900

    Initial commit

---
+++ Assets/Screipts/AudioController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    private GameControllerScript gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = GameObject
+            .FindWithTag("GameController")
+            .GetComponent<GameControllerScript>();
+    }
+
+
+    void Update()
+    {
+
+    }
+
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        ///if(tag != "UnityChan")
+        //{
+            
+            switch (tag)
+            {
+                case "cube":
+                    Debug.Log(tag);
+                    gameController.AudioOn(1);
+                    break;
+                case "ground":
+                    Debug.Log(tag);
+                    gameController.AudioOn(2);
+                    break;
+                case "UnityChan":
+                    Debug.Log(tag);
+                    gameController.AudioOn(3);
+                    break;
+                default:
+                    Debug.Log(tag);
+                    //gameController.AudioOn(0.0f);
+                    break;
+            }
+        //}
+    }
+}
---
+++ Assets/Screipts/GameControllerScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour
+{
+
+    [SerializeField] public AudioClip block;
+    [SerializeField] public AudioClip oops;
+    AudioSource audioSource;
+
+    private int tagx = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AudioOn(int tagx)
+    {
+        Debug.Log(tagx);
+        if (tagx > 0)
+        {
+
+            switch (tagx)
+            {
+                case 1:
+                    Debug.Log(tagx);
+                    audioSource.PlayOneShot(block, 0.8F);
+                    break;
+                case 2:
+                    Debug.Log(tagx);
+                    audioSource.PlayOneShot(block, 0.3F);
+                    break;
+                case 3:
+                    Debug.Log(tagx);
+                    //audioSource.PlayOneShot(oops, 0.7F);
+                    break;
+                default:
+                    break;
+            }
+        }
+        /*
+       switch (tagx)
+       {
+
+           case "cube":
+               //vlm = 0.8F;
+               //Debug.Log(tagx);
+               audioSource.PlayOneShot(block, 0.8F);
+               break;
+           case "ground":
+               //vlm = 0.3F;
+               //Debug.Log(tagx);
+               audioSource.PlayOneShot(block, 0.3F);
+               break;
+           case "UnityChan":
+               //Debug.Log(tagx);
+               //audioSource.PlayOneShot(block, 0.0F);
+               break;
+           default:
+               //gameController.AudioOn(0.0f);
+               break;
+       }
+       //ブロックが衝突したらボリュームをあげる
+       //Debug.Log(vlm);
+       //audioSource.PlayOneShot(block, vlm);
+           */
+    }
+}
